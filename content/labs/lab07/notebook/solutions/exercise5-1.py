d6de51b29158df3585761c9301df7a6501935e81
Ashishakkum <119936714+Ashishakkum@users.noreply.github.com>
2023-02-09 16:42:30 +0000

    hi

--- content/labs/lab07/notebook/solutions/exercise5-1.py
+++
@@ -1,5 +0,0 @@
-loss = keras.losses.mse
-optimizer = Adam() #RMSprop(learning_rate=0.001)
-metrics = ['accuracy'] 
-
-ae_model.compile(optimizer=optimizer, loss=loss, metrics=metrics) 
--- content/labs/lab12/index.md
+++
@@ -1,9 +0,0 @@
-Title: Lab 12:
-Category: labs
-Date: 2021-01-24
-Author: 
-Slug: lab12
-Tags:
-
-
-## Slides
